902c05bcdddf4289b5acb420fe1ced0e8a202642
Pevernow <3450354617@qq.com>
2022-05-22 15:06:48 +0800

    Update pkg2appimage.sh

--- build/AppImage/pkg2appimage.sh
+++ build/AppImage/pkg2appimage.sh
@@ -4,5 +4,7 @@ if [ ! -f pkg2appimage-1803-x86_64.AppImage ]; then
     wget https://github.com/AppImage/pkg2appimage/releases/download/continuous/pkg2appimage-1803-x86_64.AppImage
     chmod +x pkg2appimage-1803-x86_64.AppImage
 fia
+
+ls -R
 ARCH=x86_64 ./pkg2appimage-1803-x86_64.AppImage ./build/AppImage/tlpui.pkg.yml
 mv out/TLP_UI-.glibc2.32-x86_64.AppImage out/TLP_UI-x86_64.AppImage
